3afd8403dc412cb90c78e09b5a13c0420cc79a30
Yubraj <iyubrajpoudel@gmail.com>
2023-05-25 01:02:39 +0545

    dotenv package added (to store & access environment variables), mongodb package added (to connect to mongodb atlas cluster), connection made and tested.

--- server.js
+++ server.js
@@ -4,6 +4,34 @@ const express = require("express");
 // express app
 const app = express();
 
+// dotenv configuration (to use environment variables stored in /.env file)
+const dotenv = require("dotenv");
+dotenv.config();
+// console.log(process.env);
+
+// connect to mongodb
+
+// requiring mongoose
+const mongoose = require("mongoose");
+
+// connect
+// mongoose.connect(<URI>, <OPTIONS>); //return promise
+
+// const MONGODB_URI = `mongodb+srv://<username>:<password>@mycluster.uecf9bg.mongodb.net/?retryWrites=true&w=majority`;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then((res)=>{
+    console.log(`Connected to MongoDB!`);
+})
+.catch((err)=>{
+    console.log(`Error occured while connecting to MongoDB!`);
+});
+
 // morgan (to track hitted endpoint in console)
 const morgan = require("morgan");
 
